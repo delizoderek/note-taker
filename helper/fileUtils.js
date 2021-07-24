8d51beb2c37c7ec78b8385b30deabcc0db501717
Derek DeLizo <delizoderek@gmail.com>
2021-07-23 21:46:04 -0700

    Updated fileUtils and apiRouter to support post requests

--- helper/fileUtils.js
+++ helper/fileUtils.js
@@ -1,25 +1,45 @@
-const fs = require('fs');
-const util = require('util');
-const path = require('path');
+const fs = require("fs");
+const util = require("util");
+const path = require("path");
 const readFile = util.promisify(fs.readFile);
 
-const readJsonFile = async (filePath) => {
-    let jsonOut = undefined;
-    try{
-        const fileData = await readFile(filePath);
-        jsonOut = JSON.parse(fileData);
-    } catch(e){
-        console.log(`Could not find file at location ${path.join(__dirname,e.path)}`);
+// const readJsonFile = (filePath) => {
+//     readFile(filePath,'utf8').then((data,err)=>{
+//         if(err){
+//             console.log(err.path);
+//             return;
+//         } else {
+//             jsonOut = JSON.parse(data);
+//             console.log(data);
+//             return jsonOut;
+//         }
+//     });
+// };
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+  fs.readFile(file, "utf8", (err, data) => {
+    if (err) {
+      console.error(`Could not find the following file ${err.path}`);
+    } else {
+      const parsedData = JSON.parse(data);
+      parsedData.push(content);
+      writeToFile(file, parsedData);
     }
-
-    return jsonOut;
-}
-
-const writeAndAppend = (filePath,data) => {
-    console.log(`${filePath}\n\n${data}`);
-}
+  });
+};
 
 module.exports = {
-    readJsonFile,
-    writeAndAppend
+    readFile,
+    readAndAppend
 };
--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -1,22 +1,28 @@
 const apiRouter = require('express').Router();
-const {readJsonFile,writeAndAppend} = require('../helper/fileUtils');
-// TODO: POST /api/notes route
+const {readFile,readAndAppend} = require('../helper/fileUtils');
 // TODO: DELETE /api/notes/:id route
 
 // GET /api/notes route
-apiRouter.get('/', async (req,res) => {
-    const noteData = await readJsonFile('./db/notes.json');
-    if(noteData){
-        res.json(noteData);
-    } else {
-        console.log(`Could not load data for ${req.method} request from ${req.baseUrl}`);
-        res.json([]);
-    }
+apiRouter.get('/', (req,res) => {
+    readFile('./db/notes.json','utf8').then((data) =>{
+        if(data){
+            console.log('Data has been loaded properly');
+            res.json(JSON.parse(data));
+        } else {
+            console.log(`Could not load data for ${req.method} request from ${req.baseUrl}`);
+            res.json([]);
+        }
+    });
 });
 
+// POST /api/notes route
 apiRouter.post('/',(req,res) => {
-    console.log('req.baseurl');
-    res.send(`${req.method} request received`);
+    if(req.body && req.body.title && req.body.text){
+        readAndAppend(req.body,"./db/notes.json");
+        res.send(`${req.method} request received and data updated`);
+    } else {
+        res.send(`Please set a note title or add text to your note`);
+    }
 });
 
 apiRouter.delete('/',(req,res) => {
